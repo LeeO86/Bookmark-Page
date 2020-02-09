9e915a51463e92674ba4964c0108608715881341
Adrian Hilber <adi.hilber@gmail.com>
2020-02-09 18:22:05 +0100

    Fixed delete empty Group Bug
    closes #14

--- www/php/deleteGroup.php
+++ www/php/deleteGroup.php
@@ -10,7 +10,18 @@ if(isset($_POST['groupname'])){
 	include 'db-conn.php';
 	$name = $_POST['groupname'];
 
-    $delete = 'DELETE `B`, `G`, `L` FROM `bookmarks` `B` INNER JOIN `link-groups-bookmarks` `L` ON `B`.`id` = `L`.`bookmark-id` INNER JOIN `groups` `G` ON `L`.`group-id` = `G`.`id` WHERE `G`.name = "'.$name.'"';
+	$query = 'SELECT COUNT(*) FROM `link-groups-bookmarks` `L` INNER JOIN `groups` `G` ON `L`.`group-id` = `G`.`id` WHERE `G`.name = "'.$name.'"';
+	if (!$result = mysqli_query($con, $query)) {
+        exit(mysqli_error($con));
+	}
+	$fetch = mysqli_fetch_assoc($result);
+
+	if ($fetch['COUNT(*)'] == 0){
+		$delete = 'DELETE FROM `groups` WHERE `groups`.`name` = "'.$name.'"';
+	}else{
+		$delete = 'DELETE `B`, `G`, `L` FROM `bookmarks` `B` INNER JOIN `link-groups-bookmarks` `L` ON `B`.`id` = `L`.`bookmark-id` INNER JOIN `groups` `G` ON `L`.`group-id` = `G`.`id` WHERE `G`.name = "'.$name.'"';
+	}
+
     if (!$result = mysqli_query($con, $delete)) {
         exit(mysqli_error($con));
     }
